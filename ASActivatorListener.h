ced27dee62422385dbffe54aa8dc3e89a0994513
Evan Petousis <evilgoldfish01@me.com>
2014-12-18 19:04:16 +1100

    Added Dynamic Selection support

---
+++ ASActivatorListener.h
@@ -0,0 +1,10 @@
+#import <libactivator/libactivator.h>
+#import <UIKit/UIKit.h>
+
+typedef void (^ASActivatorListenerEventHandler) (LAEvent *event, BOOL abortEventCalled);
+ 
+@interface ASActivatorListener : NSObject <LAListener>
+@property ASActivatorListenerEventHandler eventHandler;
++(instancetype)sharedInstance;
+- (void)loadWithEventHandler:(ASActivatorListenerEventHandler)handler;
+@end
