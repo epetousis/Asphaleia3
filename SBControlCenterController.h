34c0c27b2c2c1f3c0c6e754edf385b68f547fcce
Evan Petousis <evilgoldfish01@me.com>
2014-12-18 17:43:47 +1100

    Fixed stupid mistakes

--- SBControlCenterController.h
+++ SBControlCenterController.h
@@ -1,5 +1,7 @@
 @interface SBControlCenterController : NSObject
 +(id)sharedInstance;
+-(void)_endPresentation;
+-(void)_beginPresentation;
 -(void)presentAnimated:(BOOL)animated;
 -(void)presentAnimated:(BOOL)animated completion:(id)completion;
 -(void)_presentWithDuration:(double)duration completion:(id)completion;
@@ -8,4 +10,6 @@
 -(void)_revealSlidingViewToHeight:(float)height;
 -(void)_finishPresenting:(BOOL)presenting completion:(id)completion;
 -(void)_updateRevealPercentage:(float)percentage;
+-(void)cancelTransition;
+-(void)abortAnimatedTransition;
 @end
