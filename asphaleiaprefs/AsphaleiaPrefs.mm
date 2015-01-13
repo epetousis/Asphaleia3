6ca3426c1b257c6b66875928677ad226a6591b89
Evan Petousis <evilgoldfish01@gmail.com>
2015-01-13 18:36:23 +1100

    Made A3Tweaks icon in Settings fade in

--- asphaleiaprefs/AsphaleiaPrefs.mm
+++ asphaleiaprefs/AsphaleiaPrefs.mm
@@ -11,25 +11,26 @@
 
 @interface AsphaleiaPrefsListController: PSListController {
 	BOOL _enteredCorrectly;
-    modalPinVC *pinVC;
-    NSDate *_resignDate;
+	modalPinVC *pinVC;
+	NSDate *_resignDate;
 }
 @property BOOL passcodeViewIsTransitioning;
 @end
 
 @implementation AsphaleiaPrefsListController
 - (id)specifiers {
-    UITableView *tv = [self valueForKey:@"_table"];
-    tv.scrollEnabled = NO;
-    [self setValue:tv forKey:@"_table"];
+	UITableView *tv = [self valueForKey:@"_table"];
+	tv.scrollEnabled = NO;
+	[self setValue:tv forKey:@"_table"];
 	if(_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"AsphaleiaPrefs" target:self] retain];
 	}
 	UIBarButtonItem *nextBarButton = [[UIBarButtonItem alloc]initWithImage:[UIImage imageWithContentsOfFile:@"/Library/PreferenceBundles/AsphaleiaPrefs.bundle/NavHeart@2x.png"] style:UIBarButtonItemStyleBordered target:self action:@selector(loveMeh)];
-	[(UINavigationItem*)self.navigationItem setRightBarButtonItem:nextBarButton animated:NO];
 	UIImageView *A3ImageView = [[UIImageView alloc] initWithImage:[UIImage imageWithContentsOfFile:@"/Library/PreferenceBundles/AsphaleiaPrefs.bundle/NavA3tweaks@2x.png"]];
-	[(UINavigationItem*)self.navigationItem setLeftBarButtonItem:nil];
 	[(UINavigationItem*)self.navigationItem setTitleView:A3ImageView];
+	[(UINavigationItem*)self.navigationItem titleView].alpha = 0.0f;
+	[(UINavigationItem*)self.navigationItem setRightBarButtonItem:nextBarButton animated:NO];
+	[(UINavigationItem*)self.navigationItem setLeftBarButtonItem:nil];
 	[(UINavigationItem*)self.navigationItem setBackBarButtonItem:[[UIBarButtonItem alloc] initWithTitle:@"Asphaleia"
 			   style:UIBarButtonItemStylePlain
 			   target:nil
@@ -51,12 +52,22 @@
 	}
 }
 
--(void)viewDidAppear:(BOOL)view
+-(void)viewDidDisappear:(BOOL)animated {
+	[super viewDidDisappear:animated];
+	[(UINavigationItem*)self.navigationItem titleView].alpha = 0.0f;
+}
+
+-(void)viewDidAppear:(BOOL)animated
 {
-	[super viewDidAppear:view];
+	[super viewDidAppear:animated];
 	if (!_enteredCorrectly && !self.passcodeViewIsTransitioning) {
 		[self presentAuthView];
 		_enteredCorrectly = NO;
+	} else {
+		[(UINavigationItem*)self.navigationItem titleView].alpha = 0.0f;
+		[UIView animateWithDuration:0.3f animations:^{
+			[(UINavigationItem*)self.navigationItem titleView].alpha = 1.0f;
+		}];
 	}
 }
 
@@ -70,73 +81,73 @@
 	} else {
 		// NSLog(@"================presentAuthView set");
 		pinVC = [[modalPinVC alloc] initToSetPasscodeFirst:self];
- 		[(UIViewController *)self presentViewController:pinVC animated:YES completion:NULL];
- 	}
+		[(UIViewController *)self presentViewController:pinVC animated:YES completion:NULL];
+	}
 }
 
 - (void)goBack
 {
-    // NSLog(@"=========poping view");
-    [[[self parentController] navigationController] popViewControllerAnimated:YES];
+	// NSLog(@"=========poping view");
+	[[[self parentController] navigationController] popViewControllerAnimated:YES];
 }
 
 -(void)authenticated
 {
-    _enteredCorrectly = YES;
+	_enteredCorrectly = YES;
 }
 
 -(void)showSecurity
 {
-    [self pushController:[[securedAppsAL alloc]init]];
+	[self pushController:[[securedAppsAL alloc]init]];
 }
 
 -(void)showCreators
 {
-    [self pushController:[[creatorsVC alloc]init]];
+	[self pushController:[[creatorsVC alloc]init]];
 }
 
 -(void)showPasscodeOptions
 {
-    [self pushController:[[passcodeOptionsVC alloc]init]];
+	[self pushController:[[passcodeOptionsVC alloc]init]];
 }
 
 /*static inline void LoadDeviceKey(NSMutableDictionary *dict, NSString *key)
 {
-    CFStringRef result = (const __CFString *)@"";
-    result = (const __CFString *)MGCopyAnswer((__bridge CFStringRef)key);
-    if (result) {
-        [dict setObject:[NSString stringWithString:(__bridge NSString *)result] forKey:key];
-    }
+	CFStringRef result = (const __CFString *)@"";
+	result = (const __CFString *)MGCopyAnswer((__bridge CFStringRef)key);
+	if (result) {
+		[dict setObject:[NSString stringWithString:(__bridge NSString *)result] forKey:key];
+	}
 }*/
 
 - (void)showMailDialog
 {
-    // NSLog(@"showMailDialog");
-    if ([MFMailComposeViewController canSendMail])
-    {
-        MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
-        mailViewController.mailComposeDelegate = (id<MFMailComposeViewControllerDelegate>)self;
-        [mailViewController setSubject:@"Asphaleia Support"];
-        [mailViewController setToRecipients:[NSArray arrayWithObject:@"asphaleia@a3tweaks.com"]];
-        size_t size;
-        sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-        char *machine = (char *)malloc(size);
-        sysctlbyname("hw.machine", machine, &size, NULL, 0);
-        CFStringRef udid = (const __CFString *)MGCopyAnswer(kMGUniqueDeviceID);
-        
-        [mailViewController setMessageBody:[NSString stringWithFormat:@"\n\n UUID: %@\nDevice: %@\nFirmware: %@",udid, [NSString stringWithCString:machine encoding:NSUTF8StringEncoding], [[UIDevice currentDevice] systemVersion] ] isHTML:NO];
-        [(UINavigationController *)self presentViewController:mailViewController animated:YES completion:NULL];
-        free(machine);
-    }
-    else
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Asphaleia" message:@"Something went wrong while trying to compose the support email." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
-        [alert show];
-    }
+	// NSLog(@"showMailDialog");
+	if ([MFMailComposeViewController canSendMail])
+	{
+		MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc] init];
+		mailViewController.mailComposeDelegate = (id<MFMailComposeViewControllerDelegate>)self;
+		[mailViewController setSubject:@"Asphaleia Support"];
+		[mailViewController setToRecipients:[NSArray arrayWithObject:@"asphaleia@a3tweaks.com"]];
+		size_t size;
+		sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+		char *machine = (char *)malloc(size);
+		sysctlbyname("hw.machine", machine, &size, NULL, 0);
+		CFStringRef udid = (const __CFString *)MGCopyAnswer(kMGUniqueDeviceID);
+
+		[mailViewController setMessageBody:[NSString stringWithFormat:@"\n\n UUID: %@\nDevice: %@\nFirmware: %@",udid, [NSString stringWithCString:machine encoding:NSUTF8StringEncoding], [[UIDevice currentDevice] systemVersion] ] isHTML:NO];
+		[(UINavigationController *)self presentViewController:mailViewController animated:YES completion:NULL];
+		free(machine);
+	}
+	else
+	{
+		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Asphaleia" message:@"Something went wrong while trying to compose the support email." delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+		[alert show];
+	}
 }
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
-{              
-    [controller dismissViewControllerAnimated:YES completion:NULL];
+{
+	[controller dismissViewControllerAnimated:YES completion:NULL];
 }
 @end
 
