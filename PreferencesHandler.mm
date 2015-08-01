89a6cb8945114f64bb8a587ccf533e5d51d0a664
Evan Petousis <evan@petousis.net>
2015-08-01 23:02:07 +1000

    Fixed Touch ID monitor continuing to run on non-Touch ID devices

--- PreferencesHandler.mm
+++ PreferencesHandler.mm
@@ -48,7 +48,7 @@ BOOL isTouchIDDevice(void) {
 
     NSArray *touchIDModels = @[ @"iPhone6,1", @"iPhone6,2", @"iPhone7,1", @"iPhone7,2", @"iPad5,3", @"iPad5,4", @"iPad4,7", @"iPad4,8", @"iPad4,9" ];
 
-    if (bk)
+    if (bk && [[NSBundle mainBundle].bundleIdentifier isEqualToString:@"com.apple.springboard"])
         return [touchIDModels containsObject:results] && [[[bk manager] identities:nil] count] > 0;
 
     return [touchIDModels containsObject:results];
