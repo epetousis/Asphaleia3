16491607fc6b529930c33e86790a72ec98493bf3
Evan Petousis <evilGoldfish@users.noreply.github.com>
2015-08-16 10:40:02 +1000

    Changed Touch ID check in settings

--- asphaleiaprefs/TouchIDInfo.mm
+++ asphaleiaprefs/TouchIDInfo.mm
@@ -1,25 +1,11 @@
 #import "TouchIDInfo.h"
-#include <sys/sysctl.h>
-#import <objc/runtime.h>
-#import <dlfcn.h>
+#import <LocalAuthentication/LocalAuthentication.h>
 
 BOOL isTouchIDDevice(void) {
-	dlopen("/System/Library/PrivateFrameworks/BiometricKit.framework/BiometricKit", RTLD_LAZY);
-	Class bk = objc_getClass("BiometricKit");
+	LAContext *context = [[LAContext alloc] init];
 
-    int sysctlbyname(const char *, void *, size_t *, void *, size_t);
-
-    size_t size;
-    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-
-    char *answer = (char *)malloc(size);
-    sysctlbyname("hw.machine", answer, &size, NULL, 0);
-
-    NSString *results = [NSString stringWithCString:answer encoding: NSUTF8StringEncoding];
-
-    free(answer);
-
-    NSArray *touchIDModels = @[ @"iPhone6,1", @"iPhone6,2", @"iPhone7,1", @"iPhone7,2", @"iPad5,3", @"iPad5,4", @"iPad4,7", @"iPad4,8", @"iPad4,9" ];
-
-    return [touchIDModels containsObject:results] && [[[bk manager] identities:nil] count] > 0;
+    if (![context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
+        return NO;
+    }
+    return YES;
 }
